40289b38569ff8e00e1943c125c0480bbe89856d
ericjiji3 <eric.ji3333@gmail.com>
2023-11-04 13:55:32 -0500

    first staging commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,10 @@
 import Head from 'next/head'
 import { client } from '@/lib/contentful';
 import ContentfulImage from '@/components/ContentfulImage';
-import ContactForm from '@/components/ContactForm';
+// import ContactForm from '@/components/ContactForm';
 import Logo from '../../public/images/combinedOutlineLogo.png';
-import Give1 from '../../public/images/give1.png';
-import Give2 from '../../public/images/give2.png';
-import Give3 from '../../public/images/give3.png';
-import Give4 from '../../public/images/give4.png';
-// import Card from "@/components/Card";
-// import Intro from '@/components/Intro';
-import CountdownTimer from '@/components/CountdownTimer';
+import Card from "@/components/Card";
+import Intro from '@/components/Intro';
 // import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
@@ -31,62 +26,13 @@ export default function Home({ gallery, introductionPhotos }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* <Intro introPhotos={introductionPhotos} setIntroFinish={setIntroFinish}/> */}
+      <Intro introPhotos={introductionPhotos} setIntroFinish={setIntroFinish}/>
       <main className={introFinish ? `${styles.main} ${styles.active}` : `${styles.main}`}>
-        <p>ERROR...CONTENT UNDER CONSTRUCTION</p>
-        <CountdownTimer/>
-        <div className={styles.logo}>
-          <ContentfulImage
-              src={Logo}
-              width={500}
-              className={`${styles.photo}`}
-              quality='100'
-              alt="oops"
-              priority
-          />
-        </div>
-        <div className={styles.givePhotos}>
-        <ContentfulImage
-              src={Give3}
-              width={250}
-              className={`${styles.photo}`}
-              quality='100'
-              alt="oops"
-              priority
-          />
-          
-          <ContentfulImage
-              src={Give2}
-              width={250}
-              className={`${styles.photo}`}
-              quality='100'
-              alt="oops"
-              priority
-          />
-          <ContentfulImage
-              src={Give4}
-              width={250}
-              className={`${styles.photo}`}
-              quality='100'
-              alt="oops"
-              priority
-          />
-          <ContentfulImage
-              src={Give1}
-              width={250}
-              className={`${styles.photo}`}
-              quality='100'
-              alt="oops"
-              priority
-          />
-          
-        </div>
-        <ContactForm/>
-        {/* <div className={styles.homeContainer}>
+        <div className={styles.homeContainer}>
             {gallery.map((post, i) => (
               <Card key={i} post={post} />
             ))}
-        </div> */}
+        </div>
       </main>
     </>
   )
