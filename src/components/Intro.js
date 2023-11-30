d12096052177e0a57f60f809f94d18580b6f5519
ericjiji3 <eric.ji3333@gmail.com>
2023-11-29 19:27:54 -0600

    idf

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -173,44 +173,44 @@ const Intro = ({ introPhotos, setIntroFinish }) => {
         //             }            
         //     }
         // },
-        // onTouchStartOrOnMouseDown: ({event}) => {
-        //     setInterval(function(){
-        //         setScrollPos(scrollPos + 1);
-        //         if(scrollPos == 1){
-        //             setHideScroll(true);
-        //         }
-        //         if(activePhoto < introPhotos[0].fields.images.length - 3){
-        //             if(scrollPos % 8 == 0){
-        //                 console.log('scroll position hit: ' , scrollPos);
-        //                 setActivePhoto(scrollPos/8);
-        //                 setStep(0);
-        //             }
-        //             if(activePhoto % 2 == 0){
-        //                 setStep(scrollPos % 8);
-        //             }else{
-        //                 setStep(-(scrollPos % 8));
-        //                 console.log('odd photo: ', step);
-        //             }
-        //         }else{
-        //                 console.log(introPhotos[0].fields.images.length);
-        //                 if(activePhoto == introPhotos[0].fields.images.length - 1 && step == 70){
-        //                     console.log('ENDINGINGINGD');
-        //                     setHideIntro(true);
-        //                     setIntroFinish(true);
-        //                 }else{
-        //                     if(scrollPos % 8 == 0){
-        //                         setActivePhoto(scrollPos/8);
-        //                         setStep(10);
-        //                     }else{
-        //                         console.log('LAST 3 IMAGES');
-        //                         // setActivePhoto(scrollPos/8);
-        //                         setStep(((step / 10) + 1) * 10);
-        //                     }
+        onTouchStartOrOnMouseDown: ({event}) => {
+            setInterval(function(){
+                setScrollPos(scrollPos + 1);
+                if(scrollPos == 1){
+                    setHideScroll(true);
+                }
+                if(activePhoto < introPhotos[0].fields.images.length - 3){
+                    if(scrollPos % 8 == 0){
+                        console.log('scroll position hit: ' , scrollPos);
+                        setActivePhoto(scrollPos/8);
+                        setStep(0);
+                    }
+                    if(activePhoto % 2 == 0){
+                        setStep(scrollPos % 8);
+                    }else{
+                        setStep(-(scrollPos % 8));
+                        console.log('odd photo: ', step);
+                    }
+                }else{
+                        console.log(introPhotos[0].fields.images.length);
+                        if(activePhoto == introPhotos[0].fields.images.length - 1 && step == 70){
+                            console.log('ENDINGINGINGD');
+                            setHideIntro(true);
+                            setIntroFinish(true);
+                        }else{
+                            if(scrollPos % 8 == 0){
+                                setActivePhoto(scrollPos/8);
+                                setStep(10);
+                            }else{
+                                console.log('LAST 3 IMAGES');
+                                // setActivePhoto(scrollPos/8);
+                                setStep(((step / 10) + 1) * 10);
+                            }
                             
-        //                 }            
-        //         }
-        //     }, 500)
-        // },
+                        }            
+                }
+            }, 500)
+        },
         // onSwipedUp: (eventData) => {
         //     console.log(eventData);
         //     if(eventData.deltaY < 0){
